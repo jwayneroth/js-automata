84441340164b7d03051ece0f522dfea8415dcb4b
Jonathan Roth <jwayneroth@gmail.com>
2016-11-16 23:21:53 -0500

    running type 4A over rGB with controls

--- three/app.js
+++ three/app.js
@@ -20,30 +20,47 @@
 		///////////////////////////////////////////////////////
 
 		var _self = this;
-		var ta = new scope.TA({
-			canvas: document.getElementById('canvas')
-		});
 
 		///////////////////////////////////////////////////////
 		// methods
 		///////////////////////////////////////////////////////
 
 		this.onPlayPauseClick = function() {
-			console.log('App::onPlayPauseClick');
+			//console.log('App::onPlayPauseClick');
 			if (!ta.animeID) {
 				$('#playpause').children('span')
 					.removeClass('glyphicon-play')
 					.addClass('glyphicon-pause');
 			} else {
-				$('#playpause').children('span')
-					.removeClass('glyphicon-pause')
-					.addClass('glyphicon-play');
+				_self.setPaused();
 			}
 			ta.toggleLoop();
 		};
 
 		this.onRestartClick = function() {
 			ta.restart();
+			_self.setPaused();
+		}
+
+		this.onScaleChange = function() {
+			//console.log('App::onScaleChange');
+			ta.setScale(parseInt($('#scale').val()));
+			_self.setPaused();
+		};
+
+		this.onSeedsChange = function() {
+
+			ta.setSeeds(parseInt($('#seeds').val()));
+			_self.setPaused();
+
+		}
+
+		this.onColorChange = function() {
+			ta.setColors($('#colorpicker1 input').val(), $('#colorpicker2 input').val());
+			_self.setPaused();
+		}
+
+		this.setPaused = function() {
 			$('#playpause').children('span')
 					.removeClass('glyphicon-pause')
 					.addClass('glyphicon-play');
@@ -54,8 +71,20 @@
 		 */
 		this.docReady = function() {
 			console.log('App::docReady');
+			$(".pick-a-color")
+			.pickAColor({
+				showSpectrum: false,
+				showSavedColors: false,
+				showBasicColors: false,
+				showHexInput: false,
+				allowBlank: false
+			})
+			.on('change', _self.onColorChange);
 			$('#playpause').on('click', _self.onPlayPauseClick);
 			$('#restart').on('click', _self.onRestartClick);
+			$('#scale').on('change', _self.onScaleChange);
+			$('#seeds').on('change', _self.onSeedsChange);
+			$('#playpause').click();
 		};
 
 		/**
@@ -72,10 +101,18 @@
 
 		$(document).ready(function($) {
 			_self.docReady();
-		}); /* end of as page load scripts */
+		});
 
 		$(window).load(function($) {
 			_self.windowLoaded();
+		});
+
+		var ta = new scope.TA({
+			canvas: document.getElementById('canvas'),
+			scale: parseInt($('#scale').val()),
+			seeds: parseInt($('#seeds').val()),
+			color_one: $('#colorpicker1').val(),
+			color_two: $('#colorpicker2').val()
 		});
 
 	}; // end init
--- three/ta.js
+++ three/ta.js
@@ -2,21 +2,28 @@
 
 	function TA(config) {
 
-		config = config || {};
+		var config = config || {};
 
-		defaults = {
-			scale: 1,
-			canvas: document.getElementById('canvas'),
+		var rgb_inc = 200;
+
+		var defaults = {
+			scale: 10,
+			seeds: .1,
+			color_one: '333',
+			color_two: 'EEE',
 			animeID: null
 		};
 
 		$.extend(this, defaults, config);
 
+		this.rgb_one = TA.hexToRGB(this.color_one);
+		this.rgb_two = TA.hexToRGB(this.color_two);
+
 		this.init();
 
 	}
 
-	var p = TA.prototype = {};
+	var p = TA.prototype = {}
 
 	/**
 	 * init
@@ -35,10 +42,7 @@
 		    width = canvas.width,
 		    height = canvas.height,
 		    imageData = ctx.getImageData(0, 0, width, height),
-		    animeID = this.animeId,
-		    scale = this.scale,
-		    rgb_one = {r:5,g:48,b:13},
-		    rgb_two = {r:224,g:212,b:215};
+		    animeID = this.animeId;
 
 		///////////////////////////////////////////////////////
 		// private methods
@@ -48,48 +52,81 @@
 		 * initLattice
 		 */
 		this.initLattice = function() {
-			var i,row,col,seed_r,seed_c,seed_offset;
 
-			for(i=0; i<imageData.data.length; i+=4) {
+			//console.log('TA::initLattice');
 
-			    row = Math.floor(i / 4 / width);
-			    col = i / 4 - row * width;
+			var i=0,
+				row=0,
+				col=0,
+				scale = _self.scale,
+				rgb_one = _self.rgb_one,
+		    	rgb_two = _self.rgb_two;
 
-			    if (row % 2 == 0) {
-			      if (col % 2 == 0) {
-			        imageData.data[i + 0] = rgb_one.r;
-			        imageData.data[i + 1] = rgb_one.g;
-			        imageData.data[i + 2] = rgb_one.b;
+			//console.log(rgb_one, rgb_two);
+
+			while( i < Math.ceil(width/scale) * Math.ceil(height/scale)) {
+
+			    //console.log('row: ' + row + '\t\t col: ' + col);
+
+			    if (row / scale % 2 == 0) {
+			      if (col / scale % 2 == 0) {
+			        ctx.fillStyle = _self.getStyle(rgb_one);
 			      } else {
-			        imageData.data[i + 0] = rgb_two.r;
-			        imageData.data[i + 1] = rgb_two.g;
-			        imageData.data[i + 2] = rgb_two.b;
+			      	ctx.fillStyle = _self.getStyle(rgb_two);
 			      }
 			    } else {
-			      if (col % 2 != 0) {
-			        imageData.data[i + 0] = rgb_one.r;
-			        imageData.data[i + 1] = rgb_one.g;
-			        imageData.data[i + 2] = rgb_one.b;
+			      if (col / scale % 2 != 0) {
+			      	ctx.fillStyle = _self.getStyle(rgb_one);
 			      } else {
-			        imageData.data[i + 0] = rgb_two.r;
-			        imageData.data[i + 1] = rgb_two.g;
-			        imageData.data[i + 2] = rgb_two.b;
+			      	ctx.fillStyle = _self.getStyle(rgb_two);
 			      }
 			    }
-			    imageData.data[i + 3] = 255;
 
-			  }
+			    ctx.moveTo(col-scale,row-scale);
+			    ctx.fillRect(col,row,scale,scale);
 
-			  for (i=0; i<Math.random() * (width * height); i++) {
-			    seed_r = Math.floor(Math.random() * height);
-			    seed_c = Math.floor(Math.random() * width);
-			    seed_offset = (seed_r * width + seed_c) * 4;
-			    imageData.data[seed_offset + 0] = (Math.random() * 100 > 50) ? rgb_one.r : rgb_two.r;
-			    imageData.data[seed_offset + 1] = (Math.random() * 100 > 50) ? rgb_one.g : rgb_two.g;
-			    imageData.data[seed_offset + 2] = (Math.random() * 100 > 50) ? rgb_one.b : rgb_two.b;
-			  }
+			  	i++;
+				if(col > width - 1 - scale) {
+					col = 0;
+					row += scale;
+				} else {
+					col += scale;
+				}
+			}
 
-			  ctx.putImageData(imageData, 0, 0);
+			_self.seedLattice();
+
+		};
+
+		/**
+		 * seedLattice
+		 */
+		this.seedLattice = function() {
+
+			var i=0,
+				scale = _self.scale,
+				seed_cnt =  _self.seeds,
+				seed_r,seed_c,seed_rgba,
+				rgb_one = _self.rgb_one,
+		    	rgb_two = _self.rgb_two;
+
+		    //console.log('TA::seedLattice \t seeds: ' + _self.seeds);
+
+			for (i=0; i<seed_cnt; i++) {
+
+			    seed_r = scale * Math.floor(Math.random() * (height/scale));
+			    seed_c = scale * Math.floor(Math.random() * (width/scale));
+			    seed_rgba = {r:rgb_one.r,g:rgb_one.g,b:rgb_one.b,a:rgb_one.a};
+
+			    if (Math.round(Math.random())) seed_rgba.r = rgb_two.r;
+			    if (Math.round(Math.random())) seed_rgba.g = rgb_two.g;
+			    if (Math.round(Math.random())) seed_rgba.b = rgb_two.b;
+
+			    ctx.fillStyle = _self.getStyle(seed_rgba);
+			    ctx.moveTo(seed_c,seed_r);
+			    ctx.fillRect(seed_c,seed_r,scale,scale);
+
+			}
 		};
 
 		/**
@@ -97,27 +134,37 @@
 		 */
 		this.iterate = function() {
 
+			imageData = ctx.getImageData(0, 0, width, height);
+
 			var id = {data:imageData.data.slice(), width: width, height: height},
-				row,
-				col,
+				scale = _self.scale,
+				i=0,
+				row=0,
+				col=0,
+				draw=0,
 				rsum,gsum,bsum,
+				tp,bp,
 				l, t, r, b, drb, dlt,
+				rgb_one = _self.rgb_one,
+		    	rgb_two = _self.rgb_two,
 				rmatch = rgb_one.r * 3 + rgb_two.r * 3,
 				gmatch = rgb_one.g * 3 + rgb_two.g * 3,
 				bmatch = rgb_one.b * 3 + rgb_two.b * 3,
 				pixel;
 
-			for(i=0; i<imageData.data.length; i+=4) {
+			while( i < Math.ceil(width/scale) * Math.ceil(height/scale)) {
 
-				row = Math.floor(i / 4 / width);
-				col = i / 4 - row * width;
+				draw = 0;
 
-				t = _self.getPixelObject(id, col, row-1, width);
-				b = _self.getPixelObject(id, col, row+1, width);
-				l = _self.getPixelObject(id, col-1, row, width);
-				r = _self.getPixelObject(id, col+1, row, width);
-				drb = _self.getPixelObject(id, col+1, row+1, width);
-				dlt = _self.getPixelObject(id, col-1, row-1, width);
+				tp = (row > 0) ? row-scale : (Math.ceil(height/scale) * scale - scale);
+				bp = (row < (Math.ceil(height/scale) * scale - scale)) ? row+scale : 0;
+
+				t = _self.getPixelObject(id, col, tp, width);
+				b = _self.getPixelObject(id, col, bp, width);
+				l = _self.getPixelObject(id, col-scale, row, width);
+				r = _self.getPixelObject(id, col+scale, row, width);
+				drb = _self.getPixelObject(id, col+scale, bp, width);
+				dlt = _self.getPixelObject(id, col-scale, tp, width);
 
 				rsum = t.r + b.r + l.r + r.r + drb.r + dlt.r;
 				gsum = t.g + b.g + l.g + r.g + drb.g + dlt.g;
@@ -126,18 +173,31 @@
 				pixel = _self.getPixelObject(id,col,row,width);
 
 				if (rsum == rmatch) {
-					imageData.data[i + 0] = (pixel.r == rgb_one.r) ? rgb_two.r : rgb_one.r;
+					draw = 1;
+					pixel.r = (pixel.r == rgb_one.r) ? rgb_two.r : rgb_one.r;
 				}
 				if (gsum == gmatch) {
-					imageData.data[i + 1] = (pixel.g == rgb_one.g) ? rgb_two.g : rgb_one.g;
+					draw = 1;
+					pixel.g = (pixel.g == rgb_one.g) ? rgb_two.g : rgb_one.g;
 				}
 				if (bsum == bmatch) {
-					imageData.data[i + 2] = (pixel.b == rgb_one.b) ? rgb_two.b : rgb_one.b;
+					draw = 1;
+					pixel.b = (pixel.b == rgb_one.b) ? rgb_two.b : rgb_one.b;
+				}
+
+				if (draw) {
+					ctx.fillStyle = _self.getStyle(pixel);
+					ctx.fillRect(col,row,scale,scale);
+				}
+
+				i++;
+				if(col > width - 1 - scale) {
+					col = 0;
+					row += scale;
+				} else {
+					col += scale;
 				}
 			}
-
-			ctx.putImageData(imageData, 0, 0);
-
 		};
 
 		this.getPixelTotal = function(imageData, x, y) {
@@ -145,7 +205,7 @@
 			r = imageData.data[offset];
 			g = imageData.data[offset + 1];
 			b = imageData.data[offset + 2];
-			//a = imageData.data[offset + 3];
+			a = imageData.data[offset + 3];
 			return r + g + b;
 		}
 
@@ -157,6 +217,10 @@
 			a = imageData.data[offset + 3];
 			return "rgba(" + r + "," + g + "," + b + "," + a + ")";
 		}
+
+		this.getStyle = function(rgba) {
+			return "rgb(" + rgba.r + "," + rgba.g + "," + rgba.b + ")";
+		};
 
 		this.getPixelObject = function(imageData, x, y) {
 			var r, g, b, a, offset = x * 4 + y * 4 * imageData.width;
@@ -170,7 +234,7 @@
 		this.drawFrame = function() {
 			_self.animeID = window.requestAnimationFrame(_self.drawFrame, canvas);
 			_self.iterate();
-		}
+		};
 
 		///////////////////////////////////////////////////////
 		// public methods
@@ -200,6 +264,29 @@
         	_self.animeID = null;
 		};
 
+		this.setScale = function(scale) {
+			//console.log('TA::setScale \t: ' + scale);
+			_self.scale = scale;
+			_self.stopLoop();
+			_self.initLattice();
+		};
+
+		this.setSeeds = function(seeds) {
+			_self.seeds = seeds;
+			_self.stopLoop();
+			_self.initLattice();
+		};
+
+		this.setColors = function(c1,c2) {
+			console.log('setColors', c1, c2);
+			_self.color_one = c1;
+			_self.color_two = c2;
+			_self.rgb_one = TA.hexToRGB(c1),
+			_self.rgb_two = TA.hexToRGB(c2);
+			_self.stopLoop();
+			_self.initLattice();
+		};
+
 		///////////////////////////////////////////////////////
 		// main
 		///////////////////////////////////////////////////////
@@ -207,6 +294,20 @@
 		_self.initLattice();
 
 	}; // end init
+
+	TA.hexToRGB = function(hex) {
+	    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+	    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+	        return r + r + g + g + b + b;
+	    });
+	    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	    return result ? {
+	        r: parseInt(result[1], 16),
+	        g: parseInt(result[2], 16),
+	        b: parseInt(result[3], 16)
+	    } : null;
+	};
 
 	scope.TA = TA;
 
